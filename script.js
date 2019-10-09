c1368cb0e3aff851b677d3324e58dc545bbdf0d6
Stephanie Aurelio <stephanie.m.aurelio@gmail.com>
2019-10-09 16:58:14 -0700

    created files and page structure

---
+++ script.js
@@ -0,0 +1,13 @@
+// Clicking the "Start Quiz" button presents the user with questions and initializes the timer
+
+// Code for the timer - initialize at 75 sec and countdown to 0
+
+// Code for displaying the questions - link to the questions.js object arrays somehow
+
+// Code for the score calculation. Answering quickly and correctly results in a higher score. Answering incorrectly results in a time penal
+
+// If time runs out OR all questions are answered, user is presented with their final score
+
+// Player can enter initals to store score
+
+// Final score stored in localStorage and displayed in the VIEW HIGHSCORES link
