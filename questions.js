c07aa6e6c8f070fb302eae4368275ac0186f3255
Stephanie Aurelio <stephanie.m.aurelio@gmail.com>
2019-10-11 14:01:29 -0700

    edited functionality

--- questions.js
+++ questions.js
@@ -1,28 +1,28 @@
-var quesitons = [
+var questions = [
     {
-        title: "",
-        choices:[],
-        answer: ""
+        title: "Which of the following is a high-level, list-like object?",
+        choices:["string", "variable", "array", "script"],
+        answer: "array"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "Commonly used data typies DO NOT include:",
+        choices: ["strings", "booleans", "numbers", "alerts"],
+        answer: "alerts"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "The script source can only ever be placed in the HTML body.",
+        choices: ["true", "false"],
+        answer: "false"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "Which of the following is a block of code designed to perform a particular task?",
+        choices: ["function", "operator", "object", "array"],
+        answer: "function"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "Using 'for' does not require the use of a statement section.",
+        choices: ["true", "false"],
+        answer: "true"
     },
 
 ]
--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// Clicking the "Start Quiz" button presents the user with questions and initializes the timer
+// Declare all variables here
+var buttonStart = document.getElementById("start");
+var quizLaunch = document.getElementById("start-quiz");
+var questionOne = document.getElementById("question-1");
+var questionTwo = document.getElementById("question-2");
+var questionThree = document.getElementById("question-3");
+var questionFour = document.getElementById("question-3");
+var quizComplete = document.getElementById("quiz-complete");
 
-
-// Code for the timer - initialize at 75 sec and countdown to 0
+var highscoresLink = document.getElementById("score-leaderboard");
 var timerEl = document.getElementById("time-spent");
-
 var startTime = 75;
 
+// Code for the timer - initialize at 75 sec and countdown to 0
 function timerCountdown() {
     var timerInterval = setInterval(function() {
         startTime--;
@@ -14,6 +20,7 @@ function timerCountdown() {
         if(startTime === 0) {
             clearInterval(timerInterval);
             sendMessage();
+            return;
         }
     }, 1000);
 }
@@ -23,11 +30,23 @@ function sendMessage() {
 }
 
 // Code for displaying the questions - link to the questions.js object arrays somehow
+function launchQuiz() {
+    quizLaunch.style.display = "none";
+    questionOne.style.display = "block";
+}    
 
-// Code for the score calculation. Answering quickly and correctly results in a higher score. Answering incorrectly results in a time penal
+// Code for the score calculation. Answering quickly and correctly results in a higher score. Answering incorrectly results in a time penalized
+    // Use if/else statements to determine if time will be penalized
 
 // If time runs out OR all questions are answered, user is presented with their final score
 
 // Player can enter initals to store score
 
 // Final score stored in localStorage and displayed when user clicks on SCORE-LEADERBOARD
+
+// Clicking the "Start Quiz" button presents the user with questions and initializes the timer
+
+buttonStart.addEventListener("click", function() {
+    timerCountdown();
+    launchQuiz();
+});
